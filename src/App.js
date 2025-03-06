68252266df45709a644597edef921345415a8629
jnana sai ande <127129986+jnanasai420@users.noreply.github.com>
2025-03-06 17:51:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import List_Rendering from "./List_Rendering/List_Rendering";
+
+export default function App() {
+  return (
+    <div className="App">
+      <List_Rendering />
+    </div>
+  );
+}
